3d316883d24abdd3041005c798130a659089e3c1
Kevetic <Kevetic@gmail.com>
2024-01-31 18:58:10 -0600

    footer added for disclaimer

---
+++ app/footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="absolute bottom-10 text-center w-full text-xs">
+      Disclaimer: Please note that there is a cost associated with each
+      question, and the application may be temporarily unavailable until my
+      OpenAI wallet is replenished.
+    </div>
+  );
+}
+
+export default Footer;
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import Footer from "./footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,6 +27,7 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+        <Footer className="absolute bottom-10 text-center w-full text-xs" />
       </body>
     </html>
   );
