b7814e4bd16dff5e0131413dd526fbf6b74112b3
Kevetic <Kevetic@gmail.com>
2024-02-01 17:45:08 -0600

    user now saved to db

---
+++ app/api/register/route.ts
@@ -0,0 +1,26 @@
+import { connectToDatabase } from "@/helpers/server-helpers";
+import { NextResponse } from "next/server";
+import prisma from "@/lib/db";
+
+export async function POST(req: Request) {
+  try {
+    const { name, email, image } = await req.json();
+    await connectToDatabase();
+    const newUser = await prisma.user.create({
+      data: {
+        name,
+        email,
+        image,
+      },
+    });
+    console.log(newUser);
+    return NextResponse.json({ newUser }, { status: 201 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "error while registering" },
+      { status: 500 }
+    );
+  } finally {
+    await prisma.$disconnect();
+  }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,40 @@ import { Button } from "@/components/ui/button";
 import { useGetQuestions } from "./api/questions";
 import Questions from "@/components/questions/Questions";
 import { ReloadIcon } from "@radix-ui/react-icons";
-import { signIn, useSession } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { useEffect } from "react";
 
 export default function Home() {
   const { data: session } = useSession();
+
+  let email = session?.user?.email;
+  let name = session?.user?.name;
+  let image = session?.user?.image;
+
+  const getUser = async () => {
+    try {
+      const res = await fetch("/api/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          image,
+        }),
+      });
+
+      console.log(res);
+
+      if (res.ok) {
+        console.log(name, email, image);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const {
     quizzTopic,
     getQuestions,
@@ -23,7 +53,7 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center space-y-10 justify-around h-screen">
       <div className="absolute right-0 p-5 top-0 items-center justify-center gap-2 flex">
-        <Button onClick={() => signIn()}> Sign In</Button>
+        <Button onClick={() => signOut()}> Sign Out</Button>
         <ModeToggle />
       </div>
       {questionsArray.length == 0 ? (
@@ -39,15 +69,25 @@ export default function Home() {
               Please wait
             </Button>
           ) : (
-            <Button
-              className="w-3/2 border rounded-lg p-2 bg-primary"
-              onClick={() => {
-                setIsLoading(true);
-                getQuestions(quizzTopic);
-              }}
-            >
-              SUBMIT
-            </Button>
+            <>
+              <Button
+                className="w-3/2 border rounded-lg p-2 bg-primary"
+                onClick={() => {
+                  setIsLoading(true);
+                  getQuestions(quizzTopic);
+                }}
+              >
+                SUBMIT
+              </Button>
+              <Button
+                className="w-3/2 border rounded-lg p-2 bg-primary"
+                onClick={() => {
+                  getUser();
+                }}
+              >
+                TEST
+              </Button>
+            </>
           )}
         </div>
       ) : (
