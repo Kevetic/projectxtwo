32c97dbf118b3fe010456731e22df8fc40842a39
Kevetic <Kevetic@gmail.com>
2024-02-01 19:56:54 -0600

    deployment bug

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,20 +1,19 @@
 import { connectToDatabase } from "@/helpers/server-helpers";
 import { NextResponse } from "next/server";
-import { Prisma, PrismaClient } from "@prisma/client";
+import { PrismaClient, Prisma } from "@prisma/client";
 
+const prisma = new PrismaClient();
 export async function POST(req: Request) {
-  const prisma = new PrismaClient();
-
   try {
     const { name, email, image } = await req.json();
     await connectToDatabase();
-    let user: Prisma.UserCreateInput;
-    user = {
-      name: name,
-      email: email,
-      image: image,
-    };
-    const newUser = await prisma.user.create({ data: user });
+    const newUser = await prisma.user.create({
+      data: {
+        name,
+        email,
+        image,
+      },
+    });
     return NextResponse.json({ newUser }, { status: 201 });
   } catch (error) {
     return NextResponse.json(
